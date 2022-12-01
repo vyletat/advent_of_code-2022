ee928594821b99613e33c5a2b405554779a7cd24
Tomáš Vyleta <vyleta.tom@gmail.com>
2022-12-02 00:26:23 +0100

    01 December - typo

--- 01/01_1.py
+++ 01/01_1.py
@@ -1,17 +1,17 @@
 file_input = open("input.txt", "r")
 elf = []
 
-sum = 0
+calories_sum = 0
 # read input and if there is number add it to total and if not read next line and add total to elf
 for line in file_input:
     line = line.strip()
     if line.isdigit():
-        sum += int(line)
+        calories_sum += int(line)
     else:
-        elf.append(sum)
-        sum = 0
+        elf.append(calories_sum)
+        calories_sum = 0
         continue
-elf.append(sum)
+elf.append(calories_sum)
 print(max(elf))
 
 # find 3 max numbers in elf and sum them
